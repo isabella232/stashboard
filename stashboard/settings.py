b53b40785f766d89b92aaa96d97f1d42f60716ac
Alexis Lê-Quôc <alq@datadoghq.com>
2012-12-04 00:19:31 -0500

    Basic deployment.

--- stashboard/settings.py
+++ stashboard/settings.py
@@ -2,17 +2,17 @@ import os
 
 DEBUG = False
 
-SITE_NAME = "Stashboard"
-SITE_AUTHOR = "Colonel Mustache"
-SITE_URL = "http://stashbooard.appspot.com"
-REPORT_URL = "mailto:help@stashboard.org"
+SITE_NAME = "Datadog Status"
+SITE_AUTHOR = "Datadog"
+SITE_URL = "http://status.datadoghq.com"
+REPORT_URL = "mailto:help@datadoghq.com"
 
 # Twitter update settings
 TWITTER_CONSUMER_KEY = ''
 TWITTER_CONSUMER_SECRET = ''
 TWITTER_ACCESS_TOKEN = ''
 TWITTER_ACCESS_TOKEN_SECRET = ''
-TWITTER_HANDLE = 'stashboard_status'
+TWITTER_HANDLE = 'datadogops'
 
 # RSS Feed settings
 RSS_NUM_EVENTS_TO_FETCH = 50
